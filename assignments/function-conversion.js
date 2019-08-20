0adc542232eeaf578cff403014147fe9400bd463
Tara Gamache <tgamache.13@gmail.com>
2019-08-20 04:10:30 -0700

    stretch goal for function-conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -43,3 +43,8 @@ subtract(1,2);
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map( (num) =>{
+    return num*3;
+});
+console.log(triple);
